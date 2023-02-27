f8135a075ed03f2ec29909af489a94fe5d0f4ac5
Tyler M. Neher <tyler@neherdata.com>
2023-02-27 02:34:21 -0500

    src/ndslogo.png
    Signed-off-by: Tyler M. Neher <tyler@neherdata.com>

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import logo from './ndslogo.png';
-import './App.css';
+import ndslogo from "./ndslogo.png";
+import "./App.css";
 
 function App() {
   return (
-   <div className="App">
-	<header className="App-header">
-		<img src="{logo}" className="App-logo" alt="logo" />
-		
-		<h1>
-			Neher Data Systems
-		</h1>
-		<p>
-			106 Secretariat Ct<br />
-			Tinton Falls, NJ 07724<br />
-			United States
-		</p>
+    <div className="App">
+      <header className="App-header">
+        <img src="src/ndslogo.png" className="App-logo" alt="logo" />
 
-		<a className="App-link" href="mailto:hello@neherdata.com?cc=tyler@neherdata.com;joe@neherdata.com&subject=Contact%20Neher%20Data%20Systems%20%5Bneherdata.com%5D" target="_blank" rel="noopener noreferrer">
-			Contact Us
-		</a>
-	</header>
-</div>
+        <h1>Neher Data Systems</h1>
+        <p>
+          106 Secretariat Ct
+          <br />
+          Tinton Falls, NJ 07724
+          <br />
+          United States
+        </p>
 
+        <a
+          className="App-link"
+          href="mailto:hello@neherdata.com?cc=tyler@neherdata.com;joe@neherdata.com&subject=Contact%20Neher%20Data%20Systems%20%5Bneherdata.com%5D"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Us
+        </a>
+      </header>
+    </div>
   );
 }
 
