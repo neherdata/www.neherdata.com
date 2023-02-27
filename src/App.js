2e60304703a51ce15f48e9fd8de9a87980420943
Tyler M. Neher <tyler@neherdata.com>
2023-02-26 23:18:15 -0500

    add h1

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <h1>
+          Neher Data Systems
+        </h1>
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
