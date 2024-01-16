625fb24ae33f550891bfc460b850105baa701ec4
Tyler M. Neher <tyler@neherdata.com>
2024-01-16 10:47:28 -0500

    comment out TagManager in index.js
    Signed-off-by: Tyler M. Neher <tyler@neherdata.com>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import TagManager from "react-gtm-module";
+// import TagManager from "react-gtm-module";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
